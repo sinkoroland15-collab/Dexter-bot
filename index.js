dd36da200f28f65c352dd3b816d9a202d06c7314
sinkoroland15-collab <sinkoroland15@gmail.com>
2026-04-26 12:41:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const express = require("express");
 
+const app = express();
+app.get("/", (req, res) => res.send("Bot fut!"));
+app.listen(3000);
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log("Dexter online!");
+});
+
+client.login(process.env.TOKEN);
